71de32e1299109f9924b4553899c371425f0f0b9
carswelltech <github@carswell.tech>
2022-07-18 11:14:09 -0400

    Update types.go

--- types.go
+++ types.go
@@ -1,8 +1,14 @@
 package common
 
+import "golang.org/x/exp/constraints"
+
 type (
 	KVPair[K Key, V any] struct {
 		Key   K
+		Value V
+	}
+	ArrayElement[I constraints.Integer, V any] struct {
+		Index I
 		Value V
 	}
 )
