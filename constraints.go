cfd783669dbab2ad795871a58015712b699e7a10
carswelltech <github@carswell.tech>
2022-07-25 10:30:18 -0400

    rc 1.0.0; add some documentation

--- constraints.go
+++ constraints.go
@@ -5,9 +5,15 @@ import (
 )
 
 type (
-	Key         interface{ comparable }
+
+	// Key is a type constraint for associative arrays, and generic map types.  Defined here only for convenience and consistency across modules.
+	Key interface{ comparable }
+
+	// SortableKey is a type constraint for associative arrays, and generic types based on map, with an ordered quality. Defined here only for convenience and consistency across modules.
 	SortableKey interface{ constraints.Ordered }
-	Index       interface {
+
+	// Index is a type constraint for indices for array-like generic types. Defined here only for convenience and consistency across modules.
+	Index interface {
 		constraints.Integer | constraints.Float
 	}
 )
--- types.go
+++ types.go
@@ -3,10 +3,14 @@ package common
 import "golang.org/x/exp/constraints"
 
 type (
+
+	// KVPair is a struct to represent a key/value pair for a map-like generic type.  Useful for iterating maps.
 	KVPair[K Key, V any] struct {
 		Key   K
 		Value V
 	}
+
+	// ArrayElement is a struct to represent a key/value pair for an array-like generic type.  Useful for iterators.
 	ArrayElement[I constraints.Integer, V any] struct {
 		Index I
 		Value V
